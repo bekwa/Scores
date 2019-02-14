74f4b3eeefa029900c822946078eb1a306402d85
Bekwa <undiebekwa@gmail.com>
2019-02-14 14:02:50 +0100

    Commit for assignment

---
+++ Grade.java
@@ -0,0 +1,36 @@
+public class Grade{
+
+	public int A (int score){
+		if (score <=44)
+			System.out.println("Your grade is F .");
+				return 0;}
+	public int B (int score){
+		if (score <=54)
+			System.out.println("Your grade is E .");
+				return 0;}
+	public int C (int score){
+		if (score <=59)
+			System.out.println("Your grade is D .");
+				return 0;}
+	public int D (int score){
+		if (score <=64)
+			System.out.println("Your grade is C .");
+				return 0;}
+	public int E (int score){
+		if (score <=69)
+			System.out.println("Your grade is B .");
+				return 0;}
+	public int F (int score){
+		if (score <=100)
+			System.out.println("Your grade is A .");
+				return 0;}
+	public int H (int score){
+		if (score >100)
+			System.out.println("Invalid Score , check score and re-enter .");
+				return 0;}
+	public int I (int score){
+		if (score <=0)
+			System.out.println("Invalid Score , check score and re-enter .");
+				return 0;}
+}
+
